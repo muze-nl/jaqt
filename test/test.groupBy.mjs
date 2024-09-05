67f448b73dfb222b16a39addda6a58b9851345e6
Auke van Slooten <auke@muze.nl>
2024-09-05 09:53:44 +0200

    switched to groupBy().reduce() in addition to .select()

--- test/test.groupBy.mjs
+++ test/test.groupBy.mjs
@@ -16,7 +16,10 @@ const data = [
 				name: "Stitch",
 				type: "Cat"
 			}
-		]
+		],
+		metrics: {
+			height: 185
+		}
 	},
 	{
 		name: 'Jane',
@@ -28,43 +31,48 @@ const data = [
 				name: "Lilo",
 				type: "Dog"
 			}
-		]
+		],
+		metrics: {
+			height: "175"
+		}
 
 	}
 ]
 
-tap.test("groupby", t => {
-	let grouped = from(data).groupBy({
-		lastName: _.name
-	})
-	t.same(grouped['Doe'][0], data[0].name)
-	t.same(grouped['Doe'][1], data[1].name)
-	t.end()
-})
+// tap.test('groupBy', t => {
+// 	let grouped = from(data).groupBy(_.lastName)
+// 	t.same(grouped['Doe'][0], data[0])
+// 	t.same(grouped['Doe'][1], data[1])
+// 	t.end()
+// })
 
 tap.test("groupby-array", t => {
 	let grouped = from(data)
-	.groupBy({
-		pets: {
-			name: _
-		}
-	})
+	.groupBy(_.pets.name)
 	t.same(grouped.Lilo[0], data[0])
 	t.same(grouped.Lilo[1], data[1])
 	t.same(grouped.Stitch[0], data[0])
 	t.end()
 })
 
-tap.test("groupby-array-string", t => {
+tap.test("groupby-error", t => {
 	let grouped = from(data)
-	.groupBy({
-		pets: {
-			name: (o,a) => { a.push(o.name) }
-		}
+	.groupBy(_.pets)
+	// console.warn gives information, should it throw? choosing not to for now, so inconsistent data
+	// doesn't affect correct data.
+	t.same(grouped, {})
+	t.end()
+})
+
+tap.test('groupBy-select', t => {
+	let grouped = from(data)
+	.groupBy(_.pets.name)
+	.select({
+		name: _
 	})
-	t.equal(grouped.Lilo[0], data[0].name)
-	t.equal(grouped.Lilo[1], data[1].name)
-	t.equal(grouped.Stitch[0], data[0].name)
+	t.equal(grouped.Lilo[0].name, data[0].name)
+	t.equal(grouped.Lilo[1].name, data[1].name)
+	t.equal(grouped.Stitch[0].name, data[0].name)
 	t.end()
 })
 
@@ -93,13 +101,12 @@ const products = [
 		price: 1500,
 		color: "green"
 	}
-]	
+]
 
 tap.test("groupby-sum", t => {
 	let grouped = from(products)
-	.groupBy({
-		category: sum(_.price)
-	})
+	.groupBy(_.category)
+	.reduce(sum(_.price))
 	t.equal(grouped.Car, 75000)
 	t.equal(grouped.Bike, 3500)
 	t.end()
@@ -107,9 +114,8 @@ tap.test("groupby-sum", t => {
 
 tap.test("groupby-avg", t => {
 	let grouped = from(products)
-	.groupBy({
-		category: avg(_.price)
-	})
+	.groupBy(_.category)
+	.reduce(avg(_.price))
 	t.ok(grouped.Car==37500)
 	t.ok(grouped.Bike==1750)
 	t.end()
@@ -117,9 +123,8 @@ tap.test("groupby-avg", t => {
 
 tap.test("groupby-count", t => {
 	let grouped = from(products)
-	.groupBy({
-		category: count()
-	})
+	.groupBy(_.category)
+	.reduce(count())
 	t.ok(grouped.Car==2)
 	t.ok(grouped.Bike==2)
 	t.end()
@@ -127,9 +132,8 @@ tap.test("groupby-count", t => {
 
 tap.test("groupby-max", t => {
 	let grouped = from(products)
-	.groupBy({
-		category: max(_.price)
-	})
+	.groupBy(_.category)
+	.reduce(max(_.price))
 	t.ok(grouped.Car==50000)
 	t.ok(grouped.Bike==2000)
 	t.end()
@@ -137,9 +141,8 @@ tap.test("groupby-max", t => {
 
 tap.test("groupby-min", t => {
 	let grouped = from(products)
-	.groupBy({
-		category: min(_.price)
-	})
+	.groupBy(_.category)
+	.reduce(min(_.price))
 	t.ok(grouped.Car==25000)
 	t.ok(grouped.Bike==1500)
 	t.end()
@@ -147,20 +150,23 @@ tap.test("groupby-min", t => {
 
 tap.test("groupby-null", t => {
 	let grouped = from(null)
-	.groupBy({
-		lastName: _.name
-	})
+	.groupBy(_.name)
 	t.ok(grouped, null)
 	t.end()
 })
 
-tap.test("groupby-disting", t => {
+tap.test("groupby-distinct", t => {
 	let grouped = from(products)
-	.groupBy({
-		category: distinct(_.color)
-	})
-	console.log(grouped)
+	.groupBy(_.category)
+	.reduce(distinct(_.color))
 	t.same(Object.keys(grouped).length, 2)
 	t.end()
 })
 
+tap.test("groupby-deep", t => {
+	let grouped = from(data)
+	.groupBy(_.lastName)
+	.reduce(sum(_.metrics.height))
+	t.same(grouped.Doe, 360)
+	t.end()
+})
